4cb45548e4bcb7cbf30f0c0800648161a27cb10a
Ronit Kumar Sahu <145933979+ronit0000@users.noreply.github.com>
2025-09-04 16:21:05 +0530

    Update App.jsx

--- crud_front/src/App.jsx
+++ crud_front/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://98.84.69.79:9090/springapp1';
+const BASE_URL = 'http://44.207.54.26:9090/springapp1';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
